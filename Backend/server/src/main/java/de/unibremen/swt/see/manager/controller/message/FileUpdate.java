89f81ed5e1a086495c40f2eca6eee727649751de
Hannes Lennart Kuß <102760046+hlkuss@users.noreply.github.com>
2026-03-10 15:14:30 +0100

    docs(#12): added documentation for FileUpdate.java

--- Backend/server/src/main/java/de/unibremen/swt/see/manager/controller/message/FileUpdate.java
+++ Backend/server/src/main/java/de/unibremen/swt/see/manager/controller/message/FileUpdate.java
@@ -2,14 +2,35 @@ package de.unibremen.swt.see.manager.controller.message;
 
 import lombok.Data;
 
+/**
+ * Message object for file updates.
+ * This will be sent via livekit.
+ */
 @Data
 public class FileUpdate {
+
+    /**
+     * The filename relative to the project directory.
+     */
     private String fileName;
 
-    private String content;
-
+    /**
+     * The type of the project.
+     */
     private String projectType;
 
+    /**
+     * The new content of the file.
+     */
+    private String content;
+
+    /**
+     * Constructor.
+     *
+     * @param fileName The filename relative to the project directory.
+     * @param content The new content of the file.
+     * @param projectType The type of the project.
+     */
     public FileUpdate(String fileName, String content, String projectType) {
         this.fileName = fileName;
         this.content = content;
