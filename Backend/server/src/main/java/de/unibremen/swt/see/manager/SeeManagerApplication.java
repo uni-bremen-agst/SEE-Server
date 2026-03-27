f173f811e15236227fdcb0a8574882fbd1a8a939
Hannes Lennart Kuß <102760046+hlkuss@users.noreply.github.com>
2026-03-27 10:02:39 +0100

    fix(#12): remove old import

--- Backend/server/src/main/java/de/unibremen/swt/see/manager/SeeManagerApplication.java
+++ Backend/server/src/main/java/de/unibremen/swt/see/manager/SeeManagerApplication.java
@@ -17,7 +17,6 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 
