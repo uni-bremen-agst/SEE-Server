c22842068d90fc10dc8f25bd878873c775dbd52c
Hannes Lennart Kuß <102760046+hlkuss@users.noreply.github.com>
2026-04-21 14:43:29 +0200

    refactor(#10): migrated from eslint 9 to 10 and replaced eslint-plugin-react with @eslint-react/eslint-plugin

--- Frontend/eslint.config.js
+++ Frontend/eslint.config.js
@@ -1,25 +1,20 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
-import { fixupConfigRules } from "@eslint/compat";
+import { defineConfig } from "eslint/config";
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import eslintReact from "@eslint-react/eslint-plugin";
 
+export default defineConfig(
 
-export default [
-  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
-  { languageOptions: { parserOptions: { ecmaFeatures: { jsx: true } } } },
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  ...fixupConfigRules(pluginReactConfig),
   {
-    settings: {
-      react: {
-        version: 'detect',
-      },
-    },
-    rules: {
-      'react/react-in-jsx-scope': 'off',
-    },
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    // rules: {
+    //   semi: "error",
+    // },
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.recommended,
+      eslintReact.configs.recommended
+    ]
   },
-];
+
+);
