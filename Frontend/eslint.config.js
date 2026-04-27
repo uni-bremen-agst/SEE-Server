852571f1d4d7b2201a5a2f5bd6d386da0eb45968
Hannes Lennart Kuß <102760046+hlkuss@users.noreply.github.com>
2026-04-27 13:40:06 +0200

    style(#10): clean up eslint config

--- Frontend/eslint.config.js
+++ Frontend/eslint.config.js
@@ -4,17 +4,12 @@ import tseslint from 'typescript-eslint';
 import eslintReact from "@eslint-react/eslint-plugin";
 
 export default defineConfig(
-
   {
     files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
-    // rules: {
-    //   semi: "error",
-    // },
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
       eslintReact.configs.recommended
     ]
   },
-
 );
